ab1a2fe417cf84fd1d87fca80c0f0e5c4b9188fb
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-12-11 01:25:19 +0000

    update

---
+++ capitalize/capitalize.js
@@ -0,0 +1,4 @@
+function capitalize(string){
+  return string.split("").reverse().join("");
+};
+module.exports = capitalize;
---
+++ capitalize/capitalize.test.js
@@ -0,0 +1,4 @@
+const capitalize = require("./capitalize");
+test("String should be gnirts",() => {
+  expect(capitalize("string")).toBe("gnirts");
+});
