8edcd1ee7406e98edcc81e571de4d4ada4e9ae00
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2024-12-13 02:28:22 +0000

    Create code that passes the tests

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,4 +1,19 @@
 function analyzeArray(arr){
-  
+  let obj = {};
+  let max = arr[0];
+  let min = arr[0];
+  let curSum = 0;
+  let length = arr.length;
+  for(let i=0; i<arr.length; i++){
+    max = Math.max(max, arr[i]);
+    min = Math.min(min, arr[i]);
+    curSum += arr[i]
+  }
+  let avg = curSum / length;
+  obj.maximum = max;
+  obj.minimum = min;
+  obj.average = avg;
+  obj.len = length;
+  return obj
 }
 module.exports = analyzeArray;
